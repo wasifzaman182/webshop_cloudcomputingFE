988cf1c74b4d8ab18f6574bde8515403d10a867a
Wasif Zaman <75499379+wasifzaman182@users.noreply.github.com>
2024-08-08 05:56:09 +0200

    Implemented search

--- src/components/SeachBar/SearchBar.jsx
+++ src/components/SeachBar/SearchBar.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./searchbar.css";
-import { products } from "../../utils/products";
+import { useSelector } from "react-redux";
 // import useDebounce from "../../hooks/useDebounce";
+
 const SearchBar = ({ setFilterList }) => {
+  const products = useSelector((state) => state.products);
   const [searchWord, setSearchWord] = useState(null);
   // const debounceSearchWord = useDebounce(searchWord, 300);
   const handelChange = (input) => {
     setSearchWord(input.target.value);
     setFilterList(
       products.filter((item) =>
-        item.productName?.toLowerCase().includes(searchWord?.toLowerCase())
+        item.name?.toLowerCase().includes(searchWord?.toLowerCase())
       )
     );
   };
